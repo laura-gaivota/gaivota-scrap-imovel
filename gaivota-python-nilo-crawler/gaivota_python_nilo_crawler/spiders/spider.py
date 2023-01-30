d464bd92dcd74f32b3169fd1c719f6e8110777f9
Laura <laura.vera@gaivota.ai>
2023-01-30 10:22:14 -0300

    feat: collect data from each url

--- gaivota-python-nilo-crawler/gaivota_python_nilo_crawler/spiders/spider.py
+++ gaivota-python-nilo-crawler/gaivota_python_nilo_crawler/spiders/spider.py
@@ -1,5 +1,5 @@
 from scrapy import Request
-
+import re
 
 from gaivota_python_nilo_crawler.items import NiloItem
 
@@ -10,7 +10,7 @@ class NiloSpider(GaivotaSpider):
 
     url ="https://rural.niloimoveis.com.br/"
     name = "nilo_crawler"
-
+    r = []
     def __init__(self):
 
         __DATABASE_SCHEMA = "registry_layers"
@@ -42,11 +42,46 @@ class NiloSpider(GaivotaSpider):
             return 
         else:
             return r
-    def parse_imovel_detail(self):
+
+    def get_imovel_data_md_4(self,text,response):
+        return [re.search('<p>(.*)</p>', x).group(1) for x in response.css('div.col-md-4').extract() if text in x][0]
+
+    def get_estructura_e_caracteristicas(self):
         pass
-    def parse(self, response):
+    def parse_imovel_detail(self, response):
+        print('detail-imovel-------', response.meta.get('imovel')['url'])
+        aux = response.meta.get('imovel')
+        # description = response.css('div.mt-50 p::text').extract()
+        cidade = self.get_imovel_data_md_4('Cidade',response)
+        estado = self.get_imovel_data_md_4('Estado',response)
+        area_total  = self.get_imovel_data_md_4('Área Total',response)
+        finalidade = self.get_imovel_data_md_4('Finalidade',response)
+        aux['estado'] = estado
+        aux['cidade'] = cidade
+        aux['area_total'] = area_total
+        aux['finalidade'] = finalidade
+        estr = response.css('table.table tr')
+        feat_contain = []
+        not_contain = []
+        for c in estr:
+            trs = c.css('td ::text').extract()
+            check = c.css('td i.fa').xpath("@class").extract()
+            
+            if (trs[0].strip() != '') and (trs[0].strip() != ','):
+                if check[0].strip() != '' :
+                    if 'fa-check' in check[0]:
+
+                        feat_contain.append(trs[0].strip())
+                    else:
+                        not_contain.append(trs[0].strip())
+        aux['features_contain'] = feat_contain
+        aux['features_not_contain'] = not_contain
         
+        return self.build_items(to_build=aux, s3_path='s3_path')
         
+
+
+    def parse(self, response):        
         # s3_path = f"nilo/source_file/{self._start_time}/{file_name_with_type}"  # TODO: if necessary, complete the path
         if self.check_response_status(response):           
 
@@ -55,31 +90,33 @@ class NiloSpider(GaivotaSpider):
                 res = []
                 
                 for content in contents:
-                    print('-----')
+                    
                     aux = {}                 
                     code = content.css('span.código-imovel::text').get()
+                    aux['code'] = code
                     title = content.css('h2 *::text').get()
-                    # finalidade = content.css('p.caracteristica-imovel i').get()
+                    aux['title'] = title
                     price = self.parse_price(content.css('p.valor-imovel::text').get())
+                    aux['price'] = price
                     url = self.url[:-1]+content.css('div.area-texto a::attr(href)').extract()[0]
+                    aux['url'] = url
+                    
                     yield Request(url, callback=self.parse_imovel_detail, meta = {'imovel':aux})
-
+                    
 
                     
                 
-                # first_next_page = response.css('a.link02::attr(href)')
+                first_next_page = response.css('a.link02::attr(href)')
                 
-                # if len(first_next_page.extract()) > 0:
+                if len(first_next_page.extract()) > 0:
                     
-                #     first_next_page_url = self.url[:-1] + first_next_page.extract()[0]
-                #     print('--- first next page ---')
-                #     yield response.follow(first_next_page_url, callback=self.parse)
+                    first_next_page_url = self.url[:-1] + first_next_page.extract()[0]
+                    yield response.follow(first_next_page_url, callback=self.parse)
 
-                # next_page = response.css('[rel="next"] ::attr(href)').get()
+                next_page = response.css('[rel="next"] ::attr(href)').get()
 
-                # if next_page is not None:
-                #     print('--- next ---')
-                #     yield response.follow(next_page, callback=self.parse)
+                if next_page is not None:
+                    yield response.follow(next_page, callback=self.parse)
 
 
 
@@ -103,20 +140,22 @@ class NiloSpider(GaivotaSpider):
                 # self.logger.exception(f"Error while parsing items", exc_info=err)
 
     def build_items(self, to_build, s3_path):
-
-        # TODO: Implement build of items - example is using variable to_build. adapt if necessary
-        # TODO: If necessary, change this loop to iterate correctly through items
-        for item in to_build:
-            try:
-                data = NiloItem(id_example=to_build['id_example'],
-                                content_example=to_build['content_example'],
-                                s3_path=s3_path)
-                self.num_processed += 1
-                yield data
-            except Exception as err:
-                self.logger.exception("Error in build_item", exc_info=err)
-                self.dbutil.report_crawler_status(5, self, observations="Error in build_item",
-                                                  s3_path=s3_path)
+        
+        
+        try:
+            data = NiloItem(code=to_build['code'],title= to_build['title'],price = to_build['price'],
+                            finalidade = to_build['finalidade'], area_total = to_build['area_total'],
+                            cidade = to_build['cidade'], estado = to_build['estado'],
+                            features_contain = to_build['features_contain'],
+                            s3_path=s3_path, url = to_build['url'])
+            self.num_processed += 1
+            
+            yield data
+        except Exception as err:
+            print('ERROR',err)
+            self.logger.exception("Error in build_item", exc_info=err)
+            self.dbutil.report_crawler_status(5, self, observations="Error in build_item",
+                                                s3_path=s3_path)
 
     def close(self):
         super().close()
